fc540b7dcaff33d6b780be16171460fefa955a10
Andrea Bizzotto <bizz84@gmail.com>
2021-05-08 18:36:16 +0100

    Cleanup

--- lib/pushable_button.dart
+++ lib/pushable_button.dart
@@ -9,7 +9,6 @@ class PushableButton extends StatefulWidget {
     Key? key,
     this.child,
     required this.hslColor,
-    //required this.bottomColor,
     required this.height,
     this.elevation = 8.0,
     this.shadow,
@@ -18,7 +17,6 @@ class PushableButton extends StatefulWidget {
         super(key: key);
   final Widget? child;
   final HSLColor hslColor;
-  //final Color bottomColor;
   final double height;
   final double elevation;
   final BoxShadow? shadow;
@@ -36,29 +34,24 @@ class _PushableButtonState extends AnimationControllerState<PushableButton> {
   Offset _gestureLocation = Offset.zero;
 
   void _handleTapDown(TapDownDetails details) {
-    //print('tap down: ${details.localPosition}');
     _gestureLocation = details.localPosition;
     animationController.forward();
   }
 
   void _handleTapUp(TapUpDetails details) {
-    //print('tap up: ${details.localPosition}');
     animationController.reverse();
     widget.onPressed?.call();
   }
 
   void _handleTapCancel() {
-    //print('tap cancel');
     Future.delayed(Duration(milliseconds: 100), () {
-      if (!_isDragInProgress) {
+      if (!_isDragInProgress && mounted) {
         animationController.reverse();
       }
     });
   }
 
   void _handleDragStart(DragStartDetails details) {
-    //print(
-    //    'drag start (in progress: $_isDragInProgress): ${details.localPosition}');
     _gestureLocation = details.localPosition;
     _isDragInProgress = true;
     animationController.forward();
@@ -79,7 +72,6 @@ class _PushableButtonState extends AnimationControllerState<PushableButton> {
   }
 
   void _handleDragCancel() {
-    //print('drag cancel (in progress: $_isDragInProgress)');
     if (_isDragInProgress) {
       _isDragInProgress = false;
       animationController.reverse();
@@ -87,7 +79,6 @@ class _PushableButtonState extends AnimationControllerState<PushableButton> {
   }
 
   void _handleDragUpdate(DragUpdateDetails details) {
-    //print('horizontal drag update: ${details.localPosition}');
     _gestureLocation = details.localPosition;
   }
 
